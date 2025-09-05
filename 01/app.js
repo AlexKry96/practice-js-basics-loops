a79547a6925760cfac3915ab9ebbc64d23241a21
Oleksii Krylov <lehakrylov1996@gmail.com>
2025-09-05 10:30:48 +0200

    01-Done

--- 01/app.js
+++ 01/app.js
@@ -1,2 +1,12 @@
-const randomNumber = Math.round(Math.random() * 20);
+const randomNumber = Math.round(Math.random() * 10);
 console.log(randomNumber);
+
+if (randomNumber > 5) {
+    for (let i = 5; i <= randomNumber; i++) {
+        console.log (i);
+    }
+}else {
+    console.log ('Wylosowana liczba jest zbyt mała, aby użyć pętli')
+}
+
+
