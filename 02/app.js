1117276c59dde761a2b530648174d1137d330bb3
Oleksii Krylov <lehakrylov1996@gmail.com>
2025-09-05 11:38:44 +0200

    02-Done

--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,15 @@
 const x = 5;
 let result = 0;
+// let i = 1;
+
+for (let i = 1; i <= x; i++) {
+    result = result + i;
+}
+console.log("Suma liczb od 1 do", x, "wynosi:", result);
+
+// while (i <= x) {
+//     result = result + i;
+//     i ++;
+// }
+
+// console.log("Suma liczb od 1 do", x, "wynosi:", result);
